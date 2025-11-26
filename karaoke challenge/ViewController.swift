55903121f46c95dd482625ecf8cd3baee816c915
ashtonaught <ashtomorgan3@gmail.com>
2025-11-25 18:31:13 -0600

    Initial Commit

---
+++ karaoke challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  karaoke challenge
+//
+//  Created by Ashton Morgan on 11/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
